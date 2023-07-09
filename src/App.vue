c9450d4eb2577754204b4f20be16c328c6099e32
Marcos L <marcoslopez1895@gmail.com>
2023-07-09 13:29:50 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from '@/components/TheWelcome.vue'
+import { ref } from 'vue'
+// 
+/**
+ * 
+ */
+const nombre = ref('')
+const miEdad = ref(15)
+
+const sumarDiezAEdad = (valor) => {
+  miEdad.value += valor; 
+}
+
+const restarDiezAEdad = (valor) => {
+  miEdad.value -= valor; 
+}
+</script>
+
+<template>
+  <header>
+    <!-- {{ nombre }} -->
+    <!-- <input v-model="nombre" type="text" /> -->
+    <input v-model="miEdad" type="number" />
+    <!-- <img 
+      alt="Vue logo" class="logo" 
+      src="./assets/logo.svg" 
+      width="125" height="125" /> -->
+
+    <!-- <div class="wrapper">
+      <HelloWorld @hizo-click="sumarDiezAEdad" :edad="10" msg="tu puedes!" />
+      <HelloWorld @hizoClick="restarDiezAEdad" :edad="miEdad" msg="Empatica" />
+    </div> -->
+  </header>
+
+  <main>
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref, toRefs } from 'vue';
+
+const props = defineProps({
+  msg: {
+    type: String,
+    required: true
+  },
+  edad: Number
+}) 
+
+const emits = defineEmits(['hizoClick'])
+
+const alerta = () => {
+  emits('hizoClick', 20)
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="red">{{ edad + 1  }}</h1>
+    <h1 class="red">{{ edadMasUno }}</h1>
+    <h3 @click="alerta" style="border:1px solid red">
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+.red {
+  color: red;
+}
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
